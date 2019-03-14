b4be7a46e015e6ed80d27966e0198e154a3609f2
Keegan Millington <27042007@student.toiohomai.ac.nz>
2019-03-14 13:50:20 +1300

    first commit

---
+++ week3 loops t1/week3 loops t1/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace week3_loops_t1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int start = 3;
+            int finish = 7;
+
+            Console.WriteLine("N\t10*N\t\t100*N\t\t1000*N");
+            Console.WriteLine("***********For loop**********");
+            for (int i = start; i <= finish; i++)
+            {
+
+                Console.WriteLine($"{i}\t\t{i * 10}\t\t{i * 100}\t\t{i * 1000}");
+
+            }
+            Console.WriteLine("End of the loop.....");
+            Console.ReadKey();
+            int counter = 1;
+            while (counter <= finish)
+            {
+                Console.WriteLine($"{counter}\t\t{counter * 10}\t\t{counter * 100}\t\t{counter * 1000}");
+                counter = 3;
+            }
+            do
+            {
+                Console.WriteLine($"{counter}\t\t{counter * 10}\t\t{counter * 100}\t\t{counter * 1000}");
+                counter = 3;
+            } while (counter <= finish);
+
+
+
+
+        }
+    }
+}
---
+++ week3loopt2/week3loopt2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace week3loopt2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("7 times table:");
+            Console.WriteLine("----------------");
+            Console.WriteLine(">>> for loop <<<\t\t>>> while loop <<<\t\t>>> do while loop<<<");
+            for (int i =1; i < 13; i++)
+            {
+                Console.WriteLine($"{i} x 7 = {i*7}");
+            }
+            Console.ReadKey();
+        }
+    }
+}
